c29e3a23b3517bed1631b82a2b145470afc5d83b
Valeron2004 <32789691+Valeron2004@users.noreply.github.com>
2021-11-29 17:55:21 +0530

    Add files via upload

--- calc_main.c
+++ calc_main.c
@@ -1,15 +1,26 @@
+/*
+Purpose : To perform various calculations ranging from topics of basic arithmetic to sciences.
+Authors: Kaniel Pinto and Valeron Toscano
+Date: 27/11/21
+*/
+
 #include <stdio.h>
 #include "physics.c"
 #include "chemistry.c"
+//#include "basiccalc.c"
+//#include "mathematics.c"
 
 void main() {
 
     int cat_selected;
+    printf("-------------------------------------------------------------------\n");
     printf("-----------------------CALCULATOR BUDDY v1.0-----------------------\n");
+    printf("-------------------------------------------------------------------\n");
     printf("From basic math to complex physics problems - calculating made easy\n");
     printf("Select a category to display subcategories\n 1.Basic Arithmetic\n 2.Mathematics\n 3.Physics\n 4.Chemistry\n");
     scanf("%d", &cat_selected);
 
+    //switch statement to select corresponding category
     switch (cat_selected)
     {
     case 1:
