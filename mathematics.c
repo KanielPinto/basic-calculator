50a5ccdda8aa6246a187239cc7ba0ec6a983c951
Valeron <32789691+Valeron2004@users.noreply.github.com>
2021-11-29 20:58:03 +0530

    Add files via upload

--- mathematics.c
+++ mathematics.c
@@ -593,30 +593,30 @@ void lcmhcf()
 //function to find roots of quadratic equations
 void roots()
 {
+    double a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-    float a, b, c, d, x1, x2;
-    
-   
-    printf("Enter the coefficients: \n\n");
-    scanf("%f, %f, %f", &a, &b, &c);
+    discriminant = b * b - 4 * a * c;
 
-    d = (b*b - 4*a*c);
-
-    if (d < 0)
-    {
-        printf("Roots are not real"); 
+    // condition for real and different roots
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
     }
 
-    else if(d == 0)
-    {
-        x1 = x2 = (-b)/ 2*a;
-        printf("Answer = Roots are %f and %f", x1, x2);
+    // condition for real and equal roots
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.2lf;", root1);
     }
-    
-    else 
-    {
-        x1 = (-b + sqrt(d))/ 2*a;
-        x2 = (-b - sqrt(d))/ 2*a;
-        printf("Answer = Roots are %f and %f", x1, x2);
+
+    // if roots are not real
+    else {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
     }
+
 }
